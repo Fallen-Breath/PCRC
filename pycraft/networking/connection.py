eabba9ea606b83504ef8de9ccf5c8e8b88d5a590
Fallen_Breath <yqx791125833@gmail.com>
2020-01-19 11:29:42 +0800

    and the BrokenPipeError case can be removed

--- pycraft/networking/connection.py
+++ pycraft/networking/connection.py
@@ -560,8 +560,6 @@ class NetworkingThread(threading.Thread):
             finally:
                 with self.connection._write_lock:
                     self.connection.networking_thread = None
-        except BrokenPipeError:
-            pass
         except Exception:
             self.logger.error(traceback.format_exc().splitlines()[-1])
             self.connection.recorder.stop()
