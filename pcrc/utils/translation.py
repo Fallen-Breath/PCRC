e5554cf5ae9a448e53ed7830f530f9b4adf5e7be
Fallen-Breath <yqx791125833@gmail.com>
2022-09-30 14:18:29 +0800

    nested translation file structure

--- pcrc/utils/translation.py
+++ pcrc/utils/translation.py
@@ -12,7 +12,29 @@ class Translation:
 		self.translations: Dict[str, Dict[str, str]] = {}
 		for lang in LANGUAGES:
 			lang_file_path = 'resources/lang/{}.yml'.format(lang)
-			self.translations[lang] = YAML().load(resources_util.get_data(lang_file_path))
+			self.translations[lang] = self.__build_translation(YAML().load(resources_util.get_data(lang_file_path)))
+		l1 = list(self.translations['en_us'].keys())
+		l2 = list(self.translations['zh_cn'].keys())
+		assert len(l1) == len(l2)
+		for i in range(len(l1)):
+			if l1[i] != l2[i]:
+				print(l1[i], l2[i])
+		assert l1 == l2
+
+	@staticmethod
+	def __build_translation(yml: dict) -> dict:
+		def __build(obj: dict, path: str):
+			for key, value in obj.items():
+				child_path = key if len(path) == 0 else (path if key == '.' else path + '.' + key)
+				if isinstance(value, str):
+					translation[child_path] = value
+				elif isinstance(value, dict):
+					__build(value, child_path)
+				else:
+					raise TypeError()
+		translation = {}
+		__build(yml, '')
+		return translation
 
 	@property
 	def languages(self) -> Collection[str]:
